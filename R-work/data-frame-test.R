f792dc82535f931ea4d8a09dadc85da0900226f3
John Lin <linjo@us.ibm.com>
2018-10-22 05:05:53 -0400

    moving R code to new repo

---
+++ R-work/data-frame-test.R
@@ -0,0 +1,59 @@
+library(funnelR)
+
+###real data test
+df = read.delim("/Users/linjo/Box Sync/Morocco13/Project-MSS/Techstuff/fiche_rectum.csv",sep=",", colClasses = c(rep("character", 130)))
+length(unique(df$numdossier))
+#45
+
+keeps=c("numdossier","opérateur1","opérateur2","clavien_90","date_sortie")
+#keeps=c("opérateur1","clavien_90")
+
+df2 = df[,keeps]
+
+deadDf = df2[which(df2$clavien_90==5),]
+deadDf2 = data.frame(table(deadDf$opérateur1))
+colnames(deadDf2) = c("opérateur1","deaths")
+
+totalDf=data.frame(table(df2$opérateur1))
+colnames(totalDf) = c("opérateur1","patientCount")
+
+df3 = merge(df2,deadDf2,by=c("opérateur1"),all=T)
+#probably need to rethink how I am coming up with total patient counts and deaths since manipulation of data frame is below
+df3[is.na(df3$deaths),]$deaths=0
+
+df4 = merge(df3,totalDf,by=c("opérateur1"))
+
+keeps = c("opérateur1","patientCount","deaths")
+df5 = df4[keeps]
+df5 = unique(df5)
+df5$deathRate = df5$deaths/df5$patientCount
+
+df6=df5
+
+colnames(df6)[4] = "n2"
+colnames(df6)[2] = "d"
+
+colnames(df6)[3] = "n"
+
+
+dataSet = fundata(input=df6, 
+                       alpha=0.95, 
+                       alpha2=0.997, 
+                  benchmark=0.089,
+                       method='approximate', 
+                       step=1)
+
+
+testPlot = funplot(input=df6, 
+                       fundata=dataSet)
+
+testPlot
+#opérateur 1 is lead surgeon?
+
+counts=as.data.frame(table(df2))
+
+summary(Indometh)
+wide <- reshape(Indometh, v.names = "conc", idvar = "Subject",
+                timevar = "time",
+                direction = "wide")
+wide
---
+++ R-work/funnel-plot-test.R
@@ -0,0 +1,123 @@
+# install.packages("RJDBC")
+# library(RJDBC)
+# ordrv7 <- JDBC("oracle.jdbc.OracleDriver","/Library/Java/Extensions/ojdbc7.jar")
+# 17102018 funnel test
+install.packages("RMariaDB")
+#n
+#install.packages("RMySQL") they are phasing out RMySQL
+#install.packages("devtools")
+#install.packages("DBI")
+#install.packages("odbc")
+
+#test
+# library(DBI)
+# # Connect to my-db as defined in ~/.my.cnf
+# con <- dbConnect(RMariaDB::MariaDB(), group = "my-db")
+# 
+# dbListTables(con)
+# dbWriteTable(con, "mtcars", mtcars)
+# dbListTables(con)
+# 
+# dbListFields(con, "mtcars")
+# dbReadTable(con, "mtcars")
+# 
+# # You can fetch all results:
+# res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+# dbFetch(res)
+# dbClearResult(res)
+# 
+# # Or a chunk at a time
+# res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+# while(!dbHasCompleted(res)){
+#   chunk <- dbFetch(res, n = 5)
+#   print(nrow(chunk))
+# }
+# # Clear the result
+# dbClearResult(res)
+# 
+# # Disconnect from the database
+# dbDisconnect(con)
+# 
+# 
+# 
+# 
+
+install.packages("rjson")
+
+###Funnel Plot###
+install.packages("funnelR")
+library(funnelR)
+
+my_data  <- data.frame(id=c(1,2,3,4,5,6,7,8,9,10),
+                       sex=c('M','F','M','F','F','M','F','M','F','M'), 
+                       n=c(130,65,155,125,19,185,82,77,50,80), 
+                       d=c(150,200,300,250,50,220,100,90,400,425)
+)
+knitr::kable(my_data)
+
+my_limits   <- fundata(input=my_data, 
+                        benchmark=0.50, 
+                        alpha=0.95, 
+                        alpha2=0.98, 
+                        method='exact', 
+                        step=1)
+
+my_plot     <- funplot(input=my_data, 
+                       fundata=my_limits)
+
+my_plot
+
+#manipulation of test data for POC
+library(rjson)
+#scatter
+x$rate = x$n/x$d
+keeps = c("rate","d")
+x = x[keeps]
+x2 <- toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+#average
+keeps=c("d","benchmark")
+x=my_limits[keeps]
+colnames(x)=c("x","y")
+x2 = toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+#average
+keeps=c("d","benchmark")
+x=my_limits[keeps]
+colnames(x)=c("x","y")
+x2 = toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+#80 percentile ul
+keeps=c("d","up")
+x=my_limits[keeps]
+colnames(x)=c("x","y")
+x2 = toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+#80 percentile ll
+keeps=c("d","lo")
+x=my_limits[keeps]
+colnames(x)=c("x","y")
+x2 = toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+#95 percentile up
+keeps=c("d","lo")
+x=my_limits[keeps]
+colnames(x)=c("x","y")
+x2 = toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+#95 percentile ll
+keeps=c("d","lo")
+x=my_limits[keeps]
+colnames(x)=c("x","y")
+x2 = toJSON(unname(split(x, 1:nrow(x))))
+cat(x2)
+
+
+
+
