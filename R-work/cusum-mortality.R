a7bbb38557050308068e2befecf291f3ed6b4e7d
John Lin <linjo@us.ibm.com>
2018-11-02 09:33:50 -0400

    development work on cusum mortality in progress

--- R-work/cusum-mortality.R
+++ R-work/cusum-mortality.R
@@ -9,6 +9,9 @@ needs(funnelR)
 needs(ggplot2)
 needs(qcc)
 needs(surveillance)
+needs(spcadjust)
+needs(qcr)
+needs(bda)
 
 
 
@@ -241,12 +244,20 @@ test$clavien_score_90[which(test$clavien_score_90==5)]="1"
 test2=cusum(test, decision.interval = 4, se.shift = 1)
 
 data(pistonrings)
-detach(pistonrings)
 attach(pistonrings)
 diameter <- qcc.groups(diameter, sample)
+q <- qcc::cusum(diameter[1:25,], decision.interval = 1, se.shift = 1)
+q$std.dev
+q$statistics
+#0.01186586
+sdtest=aggregate(diameter~sample,data=pistonrings,sd)
+sdtest2=mean(sdtest$diameter[1:25])
+avgtest=aggregate(diameter~sample,data=pistonrings,mean)
 
-q <- cusum(diameter[1:25,], decision.interval = 1, se.shift = 0.001)
-q <- cusum(diameter[1:25,], decision.interval = 1, se.shift = 1)
+summary(q)
+
+q <- qcc::cusum(diameter[1:25,], decision.interval = 4, se.shift = 0)
+q <- qcc::cusum(diameter[1:25,], decision.interval = 1, se.shift = 1)
 
 summary(q)
 
@@ -263,11 +274,49 @@ if(plotType=="ucl") {
 } else if(plotType=="missing") {
   numMiss
 }
-  
+(74.0102-74.00118)/74.00118
 
 print(q$statistics)
 
 row.names(q$data)
 q$pos
 q$neg
+
+
+
+data(pistonrings)
+attach(pistonrings)
+diameter <- qcc.groups(diameter, sample)
+q <- qcc::cusum(diameter[1:25,], decision.interval = 1, se.shift = 0,sizes=sizes)
+getAnywhere(cusum)
+test=diameter[1:25,]
+test=as.matrix(test)
+sizes <- rep(5, nrow(test))
+stats <- paste("stats.", "xbar", sep = "")
+stats.xbar(test, sizes)
+#center is the mean
+#q$statistics shows the mean of each sample group
+#standard erros are calculated according to: z=(statistics - center)/(std.dev/sqrt(sizes))
+# and then shifted according toz.f <- z - se.shift/2
+
+# cusum(x, ...)data(robot)
+hst = robot$TMHSTMIN
+out = bda::bcusum(hst,mu=45)
+plot(out)
+
+test=c(60,20,0,15,20,30,20)
+sizes=c(5,100,5,5,5,5,4)
+qcc::cusum(test, decision.interval = 1, se.shift = 0,sizes=sizes)
+
+test=c(1,20,0,15,20,30,20)
+
+
+####test=final[c("id_patient","clavien_score_90")]
+
+test=final[c("id_patient","clavien_score_90")]
+test$clavien_score_90[which(test$clavien_score_90==999)]=0
+test$clavien_score_90[which(test$clavien_score_90==5)]=1
+test$clavien_score_90=as.numeric(test$clavien_score_90)
+test$rate=0.10
+test$diff=test$clavien_score_90-test$rate
 
