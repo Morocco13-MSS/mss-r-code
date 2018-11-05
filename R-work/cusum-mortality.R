ef46c0173e97c8586680e96dae9430c15b3dea0a
John Lin <linjo@us.ibm.com>
2018-11-05 05:20:29 -0500

    overall death rate used now for risk, removed missing patients from consideration

--- R-work/cusum-mortality.R
+++ R-work/cusum-mortality.R
@@ -9,30 +9,29 @@ needs(funnelR)
 needs(ggplot2)
 
 ##??remove the  (testing purposes)
-startDate = '"2018-01-01"'
-endDate = '"2019-01-01"'
-formType = '"E"'
-#userLevel is either 0 for surgeon level, 1 for unit level, or 2 for all
-userLevel = 2
-#taken as the utilisateur.id of the specific user logging in
-userId = 12
-plotType = "scatterPlot"
+# startDate = '"2018-01-01"'
+# endDate = '"2019-01-01"'
+# formType = '"E"'
+# #userLevel is either 0 for surgeon level, 1 for unit level, or 2 for all
+# userLevel = 2
+# #taken as the utilisateur.id of the specific user logging in
+# userId = 12
+# plotType = "cusumLine"
+
+#input from NodeJS (remove commenting when done testing)
+startDate=paste('"',input[[1]],'"',sep="")
+endDate=paste('"',input[[2]],'"',sep="")
+formType = paste('"',input[[3]],'"',sep="")
+userLevel = input[[4]]
+userId = input[[5]]
+plotType = input[[6]]
 
 #set the upper and lower control limits (ucl and lcl)
 ucl=3
 lcl=0
 
-#input from NodeJS (remove commenting when done testing)
-
-# startDate=paste('"',input[[1]],'"',sep="")
-# endDate=paste('"',input[[2]],'"',sep="")
-# formType = paste('"',input[[3]],'"',sep="")
-# userLevel = input[[4]]
-# userId = input[[5]]
-# plotType = input[[6]]
-
 #close all connections. only 16 can be open at one time
-lapply( dbListConnections( dbDriver( drv = "MySQL")), dbDisconnect)
+lapply(dbListConnections( dbDriver( drv = "MySQL")), dbDisconnect)
 
 #connect to the database
 mydb = dbConnect(MySQL(), user='root', password='abcd1234!', host='localhost')
@@ -96,20 +95,20 @@ patByLevel$yyyyMM=substr(patByLevel$date_creation,1,7)
 curative = df[keeps]
 curative = curative[which(curative$intitule=='Résection'&df2$valeur_item==1),]
 curative = unique(curative)
-
 #merge to patByLevel in order to filter for curative patients
-patByLevel=merge(patByLevel,curative,by=c("id_patient","id_formulaire"))
+patByLevel2=merge(patByLevel,curative,by=c("id_patient","id_formulaire"))
 keeps=c("valeur_item.x","id_patient","intitule.x","id_service.x","id_formulaire","date_creation.x","yyyyMM")
-patByLevel = patByLevel[keeps]
+patByLevel2 = patByLevel2[keeps]
+
 #remove any duplicates in case the same patient is repeated twice per a given doctor
-patByLevel2=patByLevel[!duplicated(patByLevel),]
-colnames(patByLevel2) = c("valeur_item","id_patient","intitule","id_service","id_formulaire","date_creation","yyyyMM")
+patByLevel3=patByLevel2[!duplicated(patByLevel2),]
+colnames(patByLevel3) = c("valeur_item","id_patient","intitule","id_service","id_formulaire","date_creation","yyyyMM")
 
 if(userLevel==2) #2 is overall (cusum for all results)
 {
   keeps=c("valeur_item","id_patient","id_formulaire","date_creation","yyyyMM")
-  patByLevel3 = patByLevel2[keeps]
-  colnames(patByLevel3) = c("id_level","id_patient","id_formulaire","date_creation","yyyyMM")
+  patByLevel4 = patByLevel3[keeps]
+  colnames(patByLevel4) = c("id_level","id_patient","id_formulaire","date_creation","yyyyMM")
   # the below is for ADMINs who will see the results of each unit
   # keeps=c("id_service","id_patient","id_formulaire")
   # patByLevel3 = patByLevel2[keeps]
@@ -118,16 +117,16 @@ if(userLevel==2) #2 is overall (cusum for all results)
 {
   #get the service aka unit id of the current doctor
   serviceId=unique(df$id_service[which(df$id.7==userId)])
-  patByLevel3=patByLevel2[which(patByLevel2$id_service==serviceId),]
+  patByLevel4=patByLevel3[which(patByLevel3$id_service==serviceId),]
   keeps=c("valeur_item","id_patient","id_formulaire","date_creation","yyyyMM")
-  patByLevel3 = patByLevel3[keeps]
-  colnames(patByLevel3) = c("id_level","id_patient","id_formulaire","date_creation","yyyyMM")
+  patByLevel4 = patByLevel4[keeps]
+  colnames(patByLevel4) = c("id_level","id_patient","id_formulaire","date_creation","yyyyMM")
 } else if(userLevel==0) #0 is surgeon level (cusum for only surgeon's patients)
 {
-  patByLevel3=patByLevel2[which(patByLevel2$valeur_item==doctorCode),]
+  patByLevel4=patByLevel3[which(patByLevel3$valeur_item==doctorCode),]
   keeps=c("valeur_item","id_patient","id_formulaire","date_creation","yyyyMM")
-  patByLevel3 = patByLevel3[keeps]
-  colnames(patByLevel3) = c("id_level","id_patient","id_formulaire","date_creation","yyyyMM")
+  patByLevel4 = patByLevel4[keeps]
+  colnames(patByLevel4) = c("id_level","id_patient","id_formulaire","date_creation","yyyyMM")
 }
 
 ##get number of deaths per level
@@ -138,50 +137,67 @@ df3 = df[keeps]
 df4=df3[which(df3$intitule=="Score de Clavien maximal dans les 90 jours postopératoires"),]
 #count the patients missing clavien scores at 90 days
 numMiss = length(unique(df4$id_patient[which(df4$valeur_item=="")]))
+#remove those patients from consideration
+df5=df4[-which(df4$valeur_item==""),]
 #get all patients who died
-df4=df4[which(df4$valeur_item=='5'),]
+df5=df5[which(df5$valeur_item=='5'),]
 
 #remove any duplicates in case the same patient is repeated twice per a given doctor
-df5=df4[!duplicated(df4),]
+df6=df5[!duplicated(df5),]
 keeps=c("valeur_item","id_patient","id_formulaire")
-df6 = df5[keeps]
-colnames(df6)=c("clavien_score_90","id_patient","id_formulaire")
-final = merge(patByLevel3,df6,by=c("id_patient","id_formulaire"),all.x=T)
+df7 = df6[keeps]
+colnames(df7)=c("clavien_score_90","id_patient","id_formulaire")
+final = merge(patByLevel4,df7,by=c("id_patient","id_formulaire"),all.x=T)
 
 #fill in 0 for not dead and 1 for dead
-final$clavien_score_90[which(is.na(final$clavien_score_90))] = 0
-final$clavien_score_90[which(final$clavien_score_90==5)] = 1
-#order by date_creation and id_patient
+final$deathFlag=0
+final$deathFlag[which(final$clavien_score_90==5)] = 1
+#order by date_creation and id_patient in order to get correct sequence of patients
 final=final[(order(final$date_creation,final$id_patient)),]
 
 # get total patient counts per doctor
 final=final %>% add_count(id_level)
-colnames(final)[7] = "total_patient"
+colnames(final)[8] = "total_patient"
 
 # get CUSUM
-final$rate=0.169
-final$clavien_score_90=as.numeric(final$clavien_score_90)
-final$diff=final$clavien_score_90-final$rate
+#calculate rate (overall for ALL CURATIVE patients)
+temp = merge(patByLevel3,df, by=c("id_patient","id_formulaire") )
+keeps=c("id_patient","id_formulaire","intitule.y","valeur_item.y")
+temp=temp[keeps]
+allDead=nrow(unique(temp[which(temp$intitule=="Score de Clavien maximal dans les 90 jours postopératoires"&temp$valeur_item==5),]))
+total=length(unique(patByLevel3$id_formulaire))
+final$rate=allDead/total
+final$deathFlag=as.numeric(final$deathFlag)
+final$diff=final$deathFlag-final$rate
 final$patNum=1:nrow(final)
 final=within(final, acc_sum <- cumsum(diff))
 final$acc_sum=as.numeric(final$acc_sum)
 final$ucl=rep(ucl,nrow(final))
 final$lcl=rep(lcl,nrow(final))
-cusumPlot=final[c("patNum","acc_sum")]
-colnames(cusumPlot)=c("x","y")
-toJSON(cusumPlot)
-upPlot=final[c("patNum","ucl")]
-colnames(upPlot)=c("x","y")
-toJSON(upPlot)
-loPlot=final[c("patNum","lcl")]
-colnames(loPlot)=c("x","y")
-toJSON(loPlot)
+#determine alerts
+alerts=final[which(final$acc_sum>ucl),]
+keeps=c("patNum","acc_sum")
+alerts=alerts[keeps]
+
+#test Plot in R
 plot(final$patNum,final$acc_sum,type="l",xlab="Patient Number",ylab="Cumulative Sum Mortality (Observed-Expected)",main="Cusum Plot for Mortality"
      ,sub=paste("Missing: ",numMiss,sep=""))
 lines(final$patNum,final$ucl,col="red")
 lines(final$patNum,final$lcl,col="blue")
-#determine alerts
-alerts=cusumPlot[which(cusumPlot$y>ucl),]
+points(alerts$patNum,alerts$acc_sum,col="red")
+
+#Format for NodeJS
+cusumPlot=final[c("patNum","acc_sum")]
+colnames(cusumPlot)=c("x","y")
+#toJSON(cusumPlot)
+upPlot=final[c("patNum","ucl")]
+colnames(upPlot)=c("x","y")
+#toJSON(upPlot)
+loPlot=final[c("patNum","lcl")]
+colnames(loPlot)=c("x","y")
+#toJSON(loPlot)
+colnames(alerts)=c("x","y")
+#toJSON(alerts)
 
 ###Format for NodeJS###
 if(plotType=="cusumLine") {
